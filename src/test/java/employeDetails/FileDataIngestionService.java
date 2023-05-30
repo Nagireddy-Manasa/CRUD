7e27afb50b2ce20e9d93c4813affae2dab21dca3
nagmanasa <nagmanasa@USHYDNAGMANASA3.us.deloitte.com>
2023-05-30 17:58:22 +0530

    SQL Connection

---
+++ src/test/java/employeDetails/FileDataIngestionService.java
@@ -0,0 +1,19 @@
+package employeDetails;
+
+import java.util.List;
+
+public interface FileDataIngestionService {
+   
+	//List<String> ParseFileData(String filePath);
+	
+	
+	
+	
+    public void LoadFileData(String filePath);
+	
+}
+
+
+	
+	
+
